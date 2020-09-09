17e38d1dcde9acaca3f5505ae13b087fee71d582
InappUser <tom.-.dixon@live.co.uk>
2020-09-09 15:20:05 +0200

    gh-pages

---
+++ backend/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "'contname': '$cont_name' or '${cont_name}'" 
+if [[ "$cont_name" == "vue-prod" ]]; then
+  echo "view prod!" 
+  npx serve -l 8080 
+else 
+  echo "vue-dev!" 
+  yarn serve
+fi
